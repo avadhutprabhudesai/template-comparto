11a8b0d1d9acbff122a73c01c1f08fefaf57ce07
avadhut <avadhutprabhudesai23@gmail.com>
2021-03-12 16:39:30 +0530

    Updates

---
+++ main.bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";var __webpack_modules__={579:()=>{eval("\nconsole.log('Webpack powered static site');\nconst foo = {};\nfoo.a = 42;\nfoo.b = ['hi'];\nfoo.d = 'var';\nconst {\n  a\n} = foo;\nconsole.log(a);\n\n//# sourceURL=webpack://css-grid-template-1/./src/index.js?")}},__webpack_exports__={};__webpack_modules__[579]()})();
